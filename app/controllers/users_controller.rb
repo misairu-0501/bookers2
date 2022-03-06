0b342d811feb4c4e43cf4863fd89a05acbc4cfd7
EC2 Default User <ec2-user@ip-172-31-27-223.ec2.internal>
2022-03-06 02:06:57 +0000

    [Add]guest login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :ensure_guest_user, only: [:edit]
+
   def index
     @users = User.all
     @book = Book.new
@@ -35,5 +38,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user), notic: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   resources :users, only:[:index, :show, :edit, :update]
 
   resources :books, only:[:index, :show, :edit, :create, :destroy, :update]
